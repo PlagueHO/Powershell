bce8f04027ec03652fd88ac16891636eb3b2ca94
PlagueHO <plagueho@gmail.com>
2015-04-05 19:07:02 +1200

    Update OfficeTools
    Removed requirement for PS 2.0

--- OfficeTools/Install-MSOffice.ps1
+++ OfficeTools/Install-MSOffice.ps1
@@ -1,5 +1,3 @@
-#Requires -Version 2.0
-#
 # ---------------------------------------------------------------------------------------------------------------------------
 # By:                Daniel Scott-Raynsford
 #                    http://dscottraynsford.wordpress.com/
